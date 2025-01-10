b54aeae8993e3aabe688209bfc3a56cb1ff59572
JmZeroQAQ <1932699628@qq.com>
2025-01-10 12:18:44 +0800

    add more keymap

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,26 +2,26 @@ local util = require("utils.util")
 
 local group = util.aug("JmGroup")
 
-util.au('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = group,
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+util.au("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = group,
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
-util.au('BufEnter', {
-  desc = "Lazy load my plugin's keymap",
-  group = group,
-  once = true,
-  callback = function ()
-    require("keymap.keymaps")
-  end,
+util.au("BufEnter", {
+	desc = "Lazy load my plugin's keymap",
+	group = group,
+	once = true,
+	callback = function()
+		require("keymap.keymaps")
+	end,
 })
 
-util.au('InsertEnter', {
-  group = group,
-  callback = function ()
-    require("utils.pairs").setup({})
-  end,
+util.au("InsertEnter", {
+	group = group,
+	callback = function()
+		require("utils.pairs").setup({})
+	end,
 })
