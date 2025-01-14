66bc6fd18d5b66f13010e4377dadba0150f3fa44
JmZeroQAQ <1932699628@qq.com>
2025-01-14 18:42:44 +0800

    add neo-tree and bufferline

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    event = { 'BufRead', 'BufNewFile' },
+    opts = {},
+  },
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    event = { 'BufRead', 'BufNewFile' },
+    opts = {},
+  },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim',
+    },
+    opts = {},
+    cmd = { 'Neotree' },
+  },
+}
